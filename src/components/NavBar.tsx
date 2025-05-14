f07745bfcde57bf2282429395281a6a79ed190d2
t.yureneva <taisiya.yureneva@gmail.com>
2025-05-14 12:47:17 +0300

    Remove unnecessary text

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,6 @@ import { ColorModeButton } from './ui/color-mode'
 const NavBar = () => {
     return <Box>
         <Image src="/image.png" boxSize="40px" float="left"/>
-        <Text fontSize="2xl" fontWeight="bold" ml={2} float="left">Nav</Text>
         <ColorModeButton float="right"/>
     </Box>
 }
