aa970441d88fdc85120b7b7fe145445f7ed135de
t.yureneva <taisiya.yureneva@gmail.com>
2025-05-17 19:10:27 +0300

    HW #36 - Badge with metacritic

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import api from "../services/api-client";
 import { Game, FetchGamesResponse } from "../model/FetchGamesTypes";
-import { SimpleGrid, Box, Card, Image, Text } from "@chakra-ui/react";
+import { SimpleGrid } from "@chakra-ui/react";
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
     const [games, setGames] = useState<Game[]>();
@@ -19,10 +20,9 @@ const GameGrid = () => {
             sm: 2,
             md: 3}
         } gap="5" maxHeight="80vh" overflow="auto">
-                {games?.map(g => <Box key={g.id}>
-                    <Image src={g.background_image} alt={"Image of " + g.name} height="100%"/>
-                    <Text>{g.name}</Text>
-                </Box>)}
+                {games?.map(g => <GameCard key={g.id} game={g} />
+
+            )}
         </SimpleGrid>
     );
 }
--- src/model/FetchGamesTypes.ts
+++ src/model/FetchGamesTypes.ts
@@ -1,7 +1,10 @@
+import { PlatformObject } from "./Platform";
+
 export interface Game {
     id: number;
     name: string;
     background_image: string;
+    metacritic: number;
 }
 
 export interface FetchGamesResponse {
